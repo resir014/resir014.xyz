16f7dd4bc879f5ef28bdcd628f095573916947c9
Resi Respati <resi.respati@compare88.net>
2017-09-28 10:34:28 +0700

    minor cleanups

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div>
+  <div className="container">
     <h1>404.</h1>
     <p>You've hit the void. <Link to="/">Go back.</Link></p>
   </div>
