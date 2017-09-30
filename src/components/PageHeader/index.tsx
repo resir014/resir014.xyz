c8bba128330f8dd06c79ff7bbe69a35933f4212b
Resi Respati <resir014@gmail.com>
2017-10-01 00:40:54 +0700

    added PageHeader and PageSubtitle components

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+const styles = require('./styles.module.scss')
+
+interface PageProps {
+  headerImage?: string
+}
+
+const PageHeader: React.SFC<PageProps> = ({ children }) => {
+  return (
+    <header className={styles.root}>
+      <div className={styles.headerInner}>
+        {children}
+      </div>
+    </header>
+  )
+}
+
+export default PageHeader
---
+++ src/components/PageSubtitle/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Helmet from 'react-helmet'
+
+const styles = require('./styles.module.scss')
+
+interface PageSubtitleProps {
+}
+
+const PageSubtitle: React.SFC<PageSubtitleProps> = ({ children }) => {
+  return (
+    <div className={styles.root}>
+      {children}
+    </div>
+  )
+}
+
+export default PageSubtitle
