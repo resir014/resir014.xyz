3e610251b21b06cbc304ffd52ac20e1b2204f67f
Resi Respati <resir014@gmail.com>
2017-11-12 18:36:01 +0700

    Remove plugin `gatsby-remark-autolink-headers`

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,6 @@ module.exports = {
             }
           },
           'gatsby-remark-prismjs',
-          'gatsby-remark-autolink-headers',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants'
         ]
