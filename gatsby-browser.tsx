cdb16af3e7bf0e6f74c2eeded4ddba052be52c41
Resi Respati <resi.respati@compare88.net>
2017-11-17 11:14:20 +0700

    Make store work

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ApplicationState } from './src/store'
 
 import configureStore from './src/configureStore'
 
--- src/configureStore.ts
+++ src/configureStore.ts
@@ -2,9 +2,12 @@ import { createStore, combineReducers } from 'redux'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import { ApplicationState, reducers } from './store'
 
-export default function configureStore() {
-  const allReducers = combineReducers<ApplicationState>({
-    ...reducers
-  })
-  return createStore<ApplicationState>(allReducers, devToolsEnhancer({}))
+export default function configureStore(initialState?: ApplicationState) {
+  const allReducers = buildRootReducer<ApplicationState>(reducers)
+  return createStore<ApplicationState>(allReducers, initialState, devToolsEnhancer({}))
+}
+
+function buildRootReducer<T>(allReducers: any) {
+  // Gatsby doesn't like the object spread operator
+  return combineReducers<T>(Object.assign({}, allReducers))
 }
