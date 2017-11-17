afca139b3bd16bba008d3a2d535a1944c9627b8a
Resi Respati <resi.respati@compare88.net>
2017-11-17 11:28:00 +0700

    Redo what `gatsby-plugin-glamor` did since their `gatsby-ssr.js` is overridden.

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
 import { Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { rehydrate } from 'glamor'
 import { ApplicationState } from './src/store'
 
 import configureStore from './src/configureStore'
 
 const store = configureStore()
+
+// Redo what `gatsby-plugin-glamor` did since their `gatsby-ssr.js` is overridden.
+exports.onClientEntry = () => {
+  const windowIfDefined = typeof window === 'undefined' ? null : window as any
+  if (windowIfDefined._glamor) {
+    rehydrate(windowIfDefined._glamor)
+  }
+}
 
 exports.replaceRouterComponent = ({ history }) =>
   ({ children }) => (
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,16 +1,47 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
+import { renderStaticOptimized } from 'glamor/server'
 
 import configureStore from './src/configureStore'
 
 const store = configureStore()
 
-exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
   const ConnectedBody = () => (
     <Provider store={store}>
       {bodyComponent}
     </Provider>
   )
-  replaceBodyHTMLString(renderToString(<ConnectedBody />))
+
+  // Redo what `gatsby-plugin-glamor` did since their `gatsby-ssr.js` is overridden.
+
+  let { html, css, ids } = renderStaticOptimized(() =>
+    renderToString(<ConnectedBody />)
+  )
+
+  replaceBodyHTMLString(html)
+
+  setHeadComponents([
+    (
+    <style
+      id="glamor-styles"
+      key="glamor-styles"
+      dangerouslySetInnerHTML={{ __html: css }}
+    />
+    ),
+    (
+    <script
+      id="glamor-ids"
+      key="glamor-ids"
+      dangerouslySetInnerHTML={{
+        __html: `
+        // <![CDATA[
+        window._glamor = ${JSON.stringify(ids)}
+        // ]]>
+        `,
+      }}
+    />
+    ),
+  ])
 }
