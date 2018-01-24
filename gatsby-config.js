303a16be2bdb9d0c9ef1aac4b32777c454cbd2e7
Resi Respati <resir014@gmail.com>
2018-01-24 15:03:02 +0700

    separate markdown content into their own folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'pages',
-        path: `${__dirname}/src/pages`
+        name: 'content',
+        path: `${__dirname}/src/content`
       }
     },
     {
