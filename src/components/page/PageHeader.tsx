1467fd5407029bccd42006ede6ee8314abe6b6b7
Resi Respati <resir014@gmail.com>
2018-03-04 19:01:51 +0700

    [PageHeader] don't use fixed heights

--- src/components/page/PageHeader.tsx
+++ src/components/page/PageHeader.tsx
@@ -16,22 +16,9 @@ const PageHeader: React.SFC<PageHeaderProps> = ({ className, children }) => (
 )
 
 export default styled(PageHeader)`
-  // position: absolute;
-  // top: 0;
-  // left: 0;
-  // width: 100%;
   position: relative;
-  height: 14rem;
   margin: 0;
   background: linear-gradient(to bottom right,
     ${colors.teal50}, ${colors.purple70});
   z-index: -1;
-
-  ${props => props.hasImage && media.md`
-    height: 18rem;
-  `}
-
-  ${props => props.hasImage && media.lg`
-    height: 22rem;
-  `}
 `
