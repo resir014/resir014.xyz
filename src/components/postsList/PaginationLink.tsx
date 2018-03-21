c5fa1b2399073c0a05da278ad9e95f61fa16de60
Resi Respati <resir014@gmail.com>
2018-03-22 02:03:50 +0700

    Paginate (#7)
    * added gatsby-paginate
    
    * added pagination to posts

---
+++ src/components/postsList/PaginationLink.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import Button from '../ui/Button'
+import styled from 'styled-components'
+
+interface PaginationLinkProps {
+  test: boolean
+  url: string
+  text: string
+}
+
+const StyledPaginationLink = styled(Button)`
+  margin-bottom: 1rem;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+`
+
+const PaginationLink: React.SFC<PaginationLinkProps> = ({ test, url, text }) => {
+  if (!test) {
+    return (
+      <StyledPaginationLink kind="nav-link" color="primary" size="lg" to={url}>
+        {text}
+      </StyledPaginationLink>
+    )
+  } else {
+    return (
+      <StyledPaginationLink kind="button" size="lg" disabled>
+        {text}
+      </StyledPaginationLink>
+    )
+  }
+}
+
+export default PaginationLink
