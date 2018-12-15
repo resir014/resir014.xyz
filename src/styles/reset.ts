61bcefd2b496ef8175473e4f8e89c26b6dc4ac29
Resi Respati <resir014@gmail.com>
2018-12-16 01:56:31 +0700

    third pass of redesign

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -91,7 +91,7 @@ injectGlobal`
 
   h1, h2, h3, h4, h5, h6 {
     margin-top: 1.414rem;
-    margin-bottom: .5rem;
+    margin-bottom: 1rem;
     font-family: ${fonts.serif};
     font-weight: 400;
     line-height: 1.2;
