c45e0cf51a46e9108a569fb2fe20ebd17baa83a9
Resi Respati <resir014@gmail.com>
2019-07-29 14:16:53 +0700

    use previous hero image again

--- src/components/home/HomepageHero.tsx
+++ src/components/home/HomepageHero.tsx
@@ -5,7 +5,7 @@ import Container from '../ui/Container'
 import { getEmSize } from '../../styles/mixins'
 import { pxSizes, emSizes, colors } from '../../styles/variables'
 
-import BackgroundPattern from '../../assets/images/line-in-motion.svg'
+import BackgroundPattern from '../../assets/images/architect.svg'
 
 interface HomepageThumbnailProps {
   className?: string
