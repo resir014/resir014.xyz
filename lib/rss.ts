4514c5aec7182330dbbc56f622921fafb0c49b54
Resi Respati <resir014@gmail.com>
2020-10-03 17:15:22 +0700

    fixed invalid RSS syntax

--- lib/rss.ts
+++ lib/rss.ts
@@ -14,18 +14,17 @@ export const generateRSSItem = (post: PostMetadata): string => `
   </item>
 `
 
-export const generateRSS = (posts: PostMetadata[]): string => `
-  <?xml version="1.0" encoding="utf-8"?>
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-      <title>${wrapWithCData(`Posts · ${siteMetadata.title}`)}</title>
-      <link>https://resir014.xyz/posts/</link>
-      <description>${wrapWithCData(siteMetadata.description)}</description>
-      <generator>Next.js</generator>
-      <language>${wrapWithCData('en')}</language>
-      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-      <atom:link href="https://resir014.xyz/posts/rss.xml" rel="self" type="application/rss+xml"/>
-      ${posts.map(generateRSSItem).join('')}
-    </channel>
-  </rss>
+export const generateRSS = (posts: PostMetadata[]): string => `<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${wrapWithCData(`Posts · ${siteMetadata.title}`)}</title>
+    <link>https://resir014.xyz/posts/</link>
+    <description>${wrapWithCData(siteMetadata.description)}</description>
+    <generator>Next.js</generator>
+    <language>${wrapWithCData('en')}</language>
+    <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+    <atom:link href="https://resir014.xyz/posts/rss.xml" rel="self" type="application/rss+xml"/>
+    ${posts.map(generateRSSItem).join('')}
+  </channel>
+</rss>
 `
