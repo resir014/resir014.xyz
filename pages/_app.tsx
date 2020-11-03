1e998be9ea3b9f39d8f7fa3c337a557bb09e65d1
Resi Respati <resir014@gmail.com>
2020-11-03 09:51:58 +0700

    add manifest.json + favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { CacheProvider, Global } from '@emotion/core'
 import { cache } from 'emotion'
 import nProgress from 'nprogress'
 
-import { Theme, GlobalStyles } from '~/components/chungking-core'
+import { Theme, GlobalStyles, colors } from '~/components/chungking-core'
 import nProgressStyles from '~/styles/nProgressStyles'
 import prismTheme from '~/styles/prismTheme'
 import { defaultOpenGraph, defaultTwitterCard } from '~/lib/seo'
@@ -36,6 +36,13 @@ function App({ Component, pageProps, router }: AppProps) {
     <CacheProvider value={cache}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content={colors.blue[500]} />
+        <meta name="theme-color" content={colors.blue[500]} />
       </Head>
 
       <DefaultSeo
