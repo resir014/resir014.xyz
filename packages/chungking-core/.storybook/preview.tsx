7bd60d2d72bf3bfa64983f2c42185912cbcca85b
Resi Respati <resir014@gmail.com>
2020-11-24 22:09:41 +0700

    add microbundle

--- packages/chungking-core/.storybook/preview.tsx
+++ packages/chungking-core/.storybook/preview.tsx
@@ -1,8 +1,8 @@
 // .storybook/preview.js
 import * as React from 'react'
 import { Global } from '@emotion/core'
-import { Theme } from '../components/chungking-core/Theme'
-import GlobalStyles from '../components/chungking-core/utils/globalStyles'
+import { Theme } from '../src/Theme'
+import GlobalStyles from '../src/utils/globalStyles'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
