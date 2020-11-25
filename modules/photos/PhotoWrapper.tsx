1f6feeb83fb1ed65cde0d89aa8e19e663886269d
Resi Respati <resir014@gmail.com>
2020-11-25 18:51:07 +0700

    more figure element tweaks

--- modules/photos/PhotoWrapper.tsx
+++ modules/photos/PhotoWrapper.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { BoxProps } from '@resir014/chungking-core'
+import { Box, BoxProps } from '@resir014/chungking-core'
 import { Figure } from '../markdown'
 
 interface PhotoWrapperProps extends BoxProps {
@@ -8,8 +8,8 @@ interface PhotoWrapperProps extends BoxProps {
 
 const PhotoWrapper: React.FC<PhotoWrapperProps> = ({ image, ...rest }) => {
   return (
-    <Figure my={0} overflow="hidden" boxShadow="single" {...rest}>
-      <img className="u-photo" loading="lazy" src={image} alt="" />
+    <Figure my={0} {...rest}>
+      <Box as="img" className="u-photo" loading="lazy" src={image} alt="Photo Post" />
     </Figure>
   )
 }
