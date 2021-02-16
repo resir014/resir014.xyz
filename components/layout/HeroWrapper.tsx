a9eaec74709bc1161180425313ef0344170dce00
Resi Respati <resir014@gmail.com>
2021-02-17 00:40:07 +0700

    rework homepage

--- components/layout/HeroWrapper.tsx
+++ components/layout/HeroWrapper.tsx
@@ -1,14 +1,14 @@
-import { Box, theme } from '@resir014/chungking-react'
-import { darken, transparentize } from 'polished'
+import { useTheme } from '@emotion/react'
+import { Box, Theme } from '@resir014/chungking-react'
 import * as React from 'react'
-
-import BackgroundPattern from '~/assets/images/texture.svg'
 
 interface HeroWrapperProps {
   className?: string
 }
 
 const HeroWrapper: React.FC<HeroWrapperProps> = ({ children, className }) => {
+  const theme: Theme = useTheme() as Theme
+
   return (
     <Box
       as="header"
@@ -16,15 +16,8 @@ const HeroWrapper: React.FC<HeroWrapperProps> = ({ children, className }) => {
       gridTemplateColumns={`1fr 1fr minmax(auto, ${theme.sizes.containers.md}px) 1fr 1fr`}
       position="relative"
       m={0}
-      py="xxl"
+      pt="xxl"
       px="lg"
-      backgroundColor={darken(0.05, theme.colors.black)}
-      background={`linear-gradient(0deg, ${transparentize(0.75, theme.colors.black)}, ${transparentize(
-        0.75,
-        theme.colors.ultramarine[700]
-      )}), url(${BackgroundPattern})`}
-      borderBottom="1px solid"
-      borderBottomColor="grey.900"
       className={className}
     >
       <Box display="flex" flexDirection="column" gridColumn="3/4">
--- modules/home/HomepageHero.tsx
+++ modules/home/HomepageHero.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { transparentize } from 'polished'
 
-import { theme, Heading, Text, Stack } from '@resir014/chungking-react'
+import { Heading, Text, Stack, Anchor, Paragraph } from '@resir014/chungking-react'
 
 import siteMetadata from '~/_data/siteMetadata.json'
 import { HeroWrapper } from '~/components/layout'
@@ -11,21 +10,22 @@ const HomepageHero: React.FC = () => {
 
   return (
     <HeroWrapper>
-      <Stack
-        className="p-author h-card"
-        spacing="xs"
-        p="lg"
-        backgroundColor={transparentize(0.75, theme.colors.blue[500])}
-        borderLeft="4px solid"
-        borderLeftColor="blue.500"
-        boxShadow="double"
-      >
-        <Heading as="h1" className="p-name" variant={900} color="turquoise.400">
-          {author.name}
-        </Heading>
-        <Text as="p" className="p-note" variant={500}>
-          {author.description}
-        </Text>
+      <Stack spacing="lg">
+        <Stack className="p-author h-card" spacing="xs" py="lg">
+          <Heading as="h1" className="p-name" variant={900} color="turquoise.400">
+            {author.name}
+          </Heading>
+          <Text as="p" className="p-note" variant={500}>
+            {author.description}
+          </Text>
+          <Anchor display="none" className="u-url u-uid" href={author.website}>
+            {author.website}
+          </Anchor>
+        </Stack>
+        <Paragraph>
+          <strong>Hey there, I&apos;m Resi!</strong> I&apos;m a self-taught web developer based in Jakarta, Indonesia. I aim to blur the
+          line between high-performance engineering and good design.
+        </Paragraph>
       </Stack>
     </HeroWrapper>
   )
