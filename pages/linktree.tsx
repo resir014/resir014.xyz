62feefc2d9930cf85f017d7e48b55e9c832dc058
Resi Respati <resir014@gmail.com>
2021-09-20 12:42:28 +0700

    upgrade deps + fix linktree heading

--- pages/linktree.tsx
+++ pages/linktree.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { InferGetStaticPropsType, NextPage } from 'next'
-import { Heading, Stack } from '@resir014/chungking-react'
+import { Stack } from '@resir014/chungking-react'
 import { LinktreeCategoryItem } from '~/types/default'
 import { Content, Page } from '~/components/layout'
 import { Post, PostBody, PostHeader } from '~/modules/posts'
 import { LinktreeCard, LinktreeList } from '~/modules/linktree'
+import { H2 } from '~/modules/markdown'
 
 export const getStaticProps = async () => {
   const data = await import('_data/linktree.json')
@@ -26,7 +27,7 @@ const LinktreePage: NextPage<LinktreePageProps> = ({ linktree }) => {
           <PostBody spacing="lg">
             {linktree.map((tree) => (
               <Stack spacing="md" key={tree.category}>
-                <Heading>{tree.category}</Heading>
+                <H2>{tree.category}</H2>
                 <LinktreeList>
                   {tree.items.map((item) => (
                     <LinktreeCard key={item.url} item={item} />
