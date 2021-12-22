8868efbc1f74a55f679def5c2eda9696b1f1da04
Resi Respati <resir014@gmail.com>
2021-12-22 22:44:14 +0700

    fix forwardRef

--- components/page/page-meta-item.tsx
+++ components/page/page-meta-item.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 
-export type PageMetaItemProps = React.ComponentPropsWithoutRef<'span'>;
+export type PageMetaItemProps = React.ComponentPropsWithoutRef<'div'>;
 
-export const PageMetaItem = React.forwardRef<HTMLSpanElement, PageMetaItemProps>(({ children }) => {
-  return (
-    <div className="inline-flex space-x-2 items-center">
-      <svg className="h-1.5 w-1.5 text-chungking-blue-500" fill="currentColor" viewBox="0 0 8 8">
-        <circle cx="4" cy="4" r="4" />
-      </svg>
-      <span className="inline-block text-base">{children}</span>
-    </div>
-  );
-});
+export const PageMetaItem = React.forwardRef<HTMLDivElement, PageMetaItemProps>(
+  ({ children }, ref) => {
+    return (
+      <div ref={ref} className="inline-flex space-x-2 items-center">
+        <svg className="h-1.5 w-1.5 text-chungking-blue-500" fill="currentColor" viewBox="0 0 8 8">
+          <circle cx="4" cy="4" r="4" />
+        </svg>
+        <span className="inline-block text-base">{children}</span>
+      </div>
+    );
+  }
+);
 
 PageMetaItem.displayName = 'PageMetaItem';
