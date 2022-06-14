90c28bc053ddf8df01d177de98afc9f04af74a57
Resi Respati <resir014@gmail.com>
2022-06-14 14:33:08 +0700

    fix image element in post header

--- modules/posts/post-header-image.tsx
+++ modules/posts/post-header-image.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+/* eslint-disable @next/next/no-img-element */
 import * as React from 'react';
 import { Container } from '~/components/layout';
 
@@ -12,7 +12,7 @@ export const PostHeaderImage: React.FC<PostHeaderImageProps> = ({ alt, src }) =>
     <section className="px-4 lg:px-6 pt-12">
       <Container>
         <div className="relative w-full h-full aspect-video overflow-hidden rounded-md shadow-lg">
-          <Image loading="lazy" src={src} alt={alt} layout="fill" objectFit="cover" />
+          <img loading="lazy" src={src} alt={alt} className="object-cover" />
         </div>
       </Container>
     </section>
