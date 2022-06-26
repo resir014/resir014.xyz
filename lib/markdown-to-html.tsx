7c11f2a75dba4ca54518c3cd76be5743ca50294b
Resi Respati <resir014@gmail.com>
2022-06-26 12:06:19 +0700

    replace prismjs with shiki

--- lib/markdown-to-html.tsx
+++ lib/markdown-to-html.tsx
@@ -1,37 +1,16 @@
-import prism from 'prismjs';
 import markdownit from 'markdown-it';
 import implicitFigures from 'markdown-it-implicit-figures';
-import loadPrismLanguage from './load-prism-language';
+import shiki from 'markdown-it-shiki';
 
 export function renderMarkdown(markdown: string) {
   const md = markdownit({
     html: true,
-    highlight: (code, lang) => {
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      if (!prism.languages[lang]) {
-        try {
-          loadPrismLanguage(lang);
-        } catch (e) {
-          let message = null;
-
-          if (lang === 'none') {
-            return code;
-          }
-
-          message = `unable to find prism language '${lang}' for parsing`;
-
-          console.warn(message);
-          return code;
-        }
-      }
-
-      const grammar = prism.languages[lang];
-      const highlightedCode = prism.highlight(code, grammar, lang);
-      return highlightedCode;
-    },
   });
 
   md.use(implicitFigures);
+  md.use(shiki, {
+    theme: 'github-dark',
+  });
   const result = md.render(markdown);
   return result;
 }
