424aceb1622f4642400c5683bd61159130624eee
Resi Respati <resir014@gmail.com>
2022-07-01 14:38:07 +0700

    use base shiki plugin

--- lib/markdown-to-html.tsx
+++ lib/markdown-to-html.tsx
@@ -1,18 +1,17 @@
 import markdownit from 'markdown-it';
 import implicitFigures from 'markdown-it-implicit-figures';
-import { markdownItShikiTwoslashSetup } from 'markdown-it-shiki-twoslash';
 
 export async function renderMarkdown(markdown: string) {
   const md = markdownit({
     html: true,
   });
 
-  const shiki = await markdownItShikiTwoslashSetup({
-    theme: 'github-dark',
-  });
+  const shiki = await import('markdown-it-shiki').then(mod => mod.default);
 
   md.use(implicitFigures);
-  md.use(shiki);
+  md.use(shiki, {
+    theme: 'github-dark',
+  });
   const result = md.render(markdown);
   return result;
 }
