4962c27886e610cd5f9b511147d017c25ae00d43
Resi Respati <resir014@gmail.com>
2023-07-27 11:29:59 +0700

    add download link to fonts

--- modules/design/typography-specs.tsx
+++ modules/design/typography-specs.tsx
@@ -12,12 +12,22 @@ export function TypographySpecs() {
         <TypographySpecimen mode="light" />
         <TypographySpecimen mode="dark" />
       </TypographySpecimenContainer>
+      <p>
+        <a href="https://rsms.me/inter/" target="_blank" rel="noopener noreferrer">
+          Download the Inter typeface here.
+        </a>
+      </p>
       <h3>JetBrains Mono</h3>
       <p>Used in elements where monospace text is required.</p>
       <TypographySpecimenContainer>
         <TypographySpecimen mode="light" family="mono" />
         <TypographySpecimen mode="dark" family="mono" />
       </TypographySpecimenContainer>
+      <p>
+        <a href="https://www.jetbrains.com/lp/mono/" target="_blank" rel="noopener noreferrer">
+          Download the JetBrains Mono typeface here.
+        </a>
+      </p>
     </>
   );
 }
