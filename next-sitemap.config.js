8470e3e23ecaedfa08fd23c4665fab929b9a08dd
Resi Respati <resir014@gmail.com>
2023-10-12 13:08:04 +0700

    update robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,13 @@ module.exports = {
   siteUrl:
     process.env.NEXT_PUBLIC_BASE_URL || process.env.DEPLOY_PRIME_URL || 'https://resir014.xyz',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+    ],
+  },
   exclude: ['/etc/something-amazing'],
 };
