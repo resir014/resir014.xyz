e248940f53f3126e5a3ef03db6bd8856b785ce16
Resi Respati <resir014@gmail.com>
2024-11-26 11:09:29 +0700

    fix: base url issue

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl:
-    process.env.NEXT_PUBLIC_BASE_URL || process.env.DEPLOY_PRIME_URL || 'https://resir014.xyz',
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    process.env.URL ||
+    process.env.DEPLOY_PRIME_URL ||
+    'https://resir014.xyz',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
